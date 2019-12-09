045ba70538e8c3727297eea8a286890454267660
elzster <hakim.elie1@gmail.com>
2019-12-08 19:12:56 -0500

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -39,11 +39,11 @@ d3.csv("assets/data/data.csv").then(function(healthData) {
           // Step 2: Create scale functions
     // ==============================
     var xLinearScale = d3.scaleLinear()
-      .domain([0, d3.max(healthData, d => d.poverty)])
+      .domain([8, d3.max(healthData, d => d.poverty)])
       .range([0, width]);
 
     var yLinearScale = d3.scaleLinear()
-      .domain([0, d3.max(healthData, d => d.healthcare)])
+      .domain([3, d3.max(healthData, d => d.healthcare)])
       .range([height, 0]);
     
     // Step 3: Create axis functions
@@ -63,25 +63,52 @@ d3.csv("assets/data/data.csv").then(function(healthData) {
     // Step 5: Create Circles
     // ==============================
     var circlesGroup = chartGroup.selectAll("circle")
-    .data(healthData)
-    .enter()
-    .append("circle")
-    .attr("cx", d => xLinearScale(d.poverty))
-    .attr("cy", d => yLinearScale(d.healthcare))
-    .attr("r", "15")
-    .attr("fill", "pink")
-    .attr("opacity", ".5");
+      .data(healthData)
+      .enter()
+      .append("circle")
+      .attr("cx", d => xLinearScale(d.poverty))
+      .attr("cy", d => yLinearScale(d.healthcare))
+      .attr("r", "15")
+      .attr("fill", "blue")
+      .attr("opacity", ".85");
     
     // Step 6: Initialize tool tip
     // ==============================
     var toolTip = d3.tip()
       .attr("class", "tooltip")
       .offset([80, -60])
-      .html(function(d) {
-        return (`${d.state}<br>Lacks HealthCare %: ${d.healthcare}<br>Poverty %: ${d.poverty}`);
-      });
+      .html(d => {
+        (`${d.state}<br>Lacks HealthCare %: ${d.healthcare}<br>Poverty %: ${d.poverty}`);
+        });
     // Step 7: Create tooltip in the chart
     // ==============================
     chartGroup.call(toolTip);
 
+    // Step 8: Create event listeners to display and hide the tooltip
+    // ==============================
+    //refers to "this element"
+
+    circlesGroup.on("click", d => {
+      toolTip.show(d, this);
+    })
+      // onmouseout event
+      .on("mouseout", function(data, index) {
+        toolTip.hide(data);
+      });
+      // Create axes labels
+      chartGroup.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left + 40)
+        .attr("x", 0 - (height / 2))
+        .attr("dy", "1em")
+        .attr("class", "axisText")
+        .text("Lacks Healthcare (%)");
+
+      chartGroup.append("text")
+        .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+        .attr("class", "axisText")
+        .text("Lacks Healthcare %");
+    }).catch(function(error) {
+      console.log(error);
 });
+    
